2f1b570517cb2937fadfaa7e19687eda7da7d5b3
Hexiang Qi <qihexiang@outlook.com>
2023-09-13 01:01:26 +0800

    make sure execute in cgroup

--- src/bin/vertex.rs
+++ src/bin/vertex.rs
@@ -1,21 +1,28 @@
 use std::{
+    collections::HashMap,
     env,
     net::SocketAddr,
     sync::{Arc, RwLock},
-    time::{SystemTime, UNIX_EPOCH}, collections::HashMap,
+    time::{SystemTime, UNIX_EPOCH}, process,
 };
 
 use axum::{
     extract::State,
+    headers::{authorization::Basic, Authorization},
     routing::{get, post},
-    Json, Router, TypedHeader, headers::{authorization::Basic, Authorization},
+    Json, Router, TypedHeader,
 };
 use cgroups_rs::CgroupPid;
-use job_dispatcher::{jobs::{JobConfiguration, ProcessStatus}, resources::Resources, http_utils::basic_check, config::load_config};
+use job_dispatcher::jobs::JobStatus;
+use job_dispatcher::{
+    config::load_config,
+    http_utils::basic_check,
+    jobs::{JobConfiguration, ProcessStatus},
+    resources::Resources,
+};
 use serde::{Deserialize, Serialize};
 use tokio::process::Command;
 use uuid::Uuid;
-use job_dispatcher::jobs::JobStatus;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct VertexConfiguration {
@@ -25,11 +32,11 @@ struct VertexConfiguration {
     ip: [u8; 4],
     #[serde(default = "default_port")]
     port: u16,
-    user_table: HashMap<String, String>
+    user_table: HashMap<String, String>,
 }
 
-fn listen_all() -> [u8;4] {
-    [0,0,0,0]
+fn listen_all() -> [u8; 4] {
+    [0, 0, 0, 0]
 }
 
 fn default_port() -> u16 {
@@ -55,23 +62,43 @@ impl VertexState {
 async fn main() {
     if let Some(executor_data) = env::args().collect::<Vec<_>>().get(1) {
         let job_configuration: JobConfiguration = serde_json::from_str(&executor_data).unwrap();
+        let task_id = Uuid::new_v4();
+        let hier = cgroups_rs::hierarchies::auto();
+        let cg = cgroups_rs::cgroup_builder::CgroupBuilder::new(&task_id.to_string())
+            .cpu()
+            .shares(job_configuration.resources.get_countable("cpu"))
+            .done()
+            .memory()
+            .memory_hard_limit(job_configuration.resources.get_countable("memory") as i64)
+            .done()
+            .build(hier)
+            .map_err(|e| e.to_string())
+            .unwrap();
+        cg.add_task_by_tgid(CgroupPid::from(process::id() as u64)).unwrap();
         job_configuration.execute().await.unwrap();
+        cg.move_task_to_parent_by_tgid(CgroupPid::from(process::id() as u64)).unwrap();
+        cg.delete().unwrap();
     } else {
-        let configuration: VertexConfiguration = if let Ok(config_path) = env::var("VERTEX_CONFIG_PATH") {
-            load_config(vec![&config_path])
-        } else {
-            load_config(vec![
-                "./vertex.yml",
-                "/etc/vertex.yml",
-                "/usr/local/etc/vertex.yml"
-            ])
-        }.expect("No validate config file found.");
+        let configuration: VertexConfiguration =
+            if let Ok(config_path) = env::var("VERTEX_CONFIG_PATH") {
+                load_config(vec![&config_path])
+            } else {
+                load_config(vec![
+                    "./vertex.yml",
+                    "/etc/vertex.yml",
+                    "/usr/local/etc/vertex.yml",
+                ])
+            }
+            .expect("No validate config file found.");
         let state = VertexState::new(&configuration.resources);
         let app = Router::new()
             .route("/free", get(get_free_resouces))
             .route("/jobs", get(get_jobs))
             .route("/job", post(execute_job))
-            .layer(axum::middleware::from_fn_with_state(configuration.user_table.clone(), basic_check))
+            .layer(axum::middleware::from_fn_with_state(
+                configuration.user_table.clone(),
+                basic_check,
+            ))
             .with_state(state);
 
         let addr = SocketAddr::from((configuration.ip, configuration.port));
@@ -82,8 +109,18 @@ async fn main() {
     }
 }
 
-async fn get_jobs(state: State<VertexState>, TypedHeader(Authorization(basic)): TypedHeader<Authorization<Basic>>) -> axum::Json<Vec<JobStatus>> {
-    let jobs = state.jobs.read().unwrap().iter().filter(|job| job.basic_user == basic.username()).map(|job| job.clone()).collect::<Vec<_>>();
+async fn get_jobs(
+    state: State<VertexState>,
+    TypedHeader(Authorization(basic)): TypedHeader<Authorization<Basic>>,
+) -> axum::Json<Vec<JobStatus>> {
+    let jobs = state
+        .jobs
+        .read()
+        .unwrap()
+        .iter()
+        .filter(|job| job.basic_user == basic.username())
+        .map(|job| job.clone())
+        .collect::<Vec<_>>();
     Json(jobs)
 }
 
@@ -111,28 +148,28 @@ async fn execute_job(
     jobs.push(new_job.clone());
     let jobs = state.jobs.clone();
     tokio::spawn(async move {
-        let hier = cgroups_rs::hierarchies::auto();
-        let cg = cgroups_rs::cgroup_builder::CgroupBuilder::new(&task_id.to_string())
-            .cpu()
-            .shares(job_configuration.resources.get_countable("cpu"))
-            .done()
-            .memory()
-            .memory_hard_limit(job_configuration.resources.get_countable("memroy") as i64)
-            .done()
-            .build(hier)
-            .map_err(|e| e.to_string())?;
+        // let hier = cgroups_rs::hierarchies::auto();
+        // let cg = cgroups_rs::cgroup_builder::CgroupBuilder::new(&task_id.to_string())
+        //     .cpu()
+        //     .shares(job_configuration.resources.get_countable("cpu"))
+        //     .done()
+        //     .memory()
+        //     .memory_hard_limit(job_configuration.resources.get_countable("memroy") as i64)
+        //     .done()
+        //     .build(hier)
+        //     .map_err(|e| e.to_string())?;
         if let Ok(mut sub_process) = Command::new(vertex).arg(executor_data).spawn() {
-            if let Ok(_) = cg.add_task(CgroupPid::from(sub_process.id().unwrap() as u64)) {
-                sub_process.wait().await.map_err(|e| e.to_string())?;
-                let mut jobs = jobs.write().unwrap();
-                let id = jobs
-                    .iter()
-                    .position(|item| item.task_id == task_id)
-                    .unwrap();
-                jobs.remove(id);
-            }
+            println!("subprocess created");
+            sub_process.wait().await.map_err(|e| e.to_string())?;
+            let mut jobs = jobs.write().unwrap();
+            let id = jobs
+                .iter()
+                .position(|item| item.task_id == task_id)
+                .unwrap();
+            jobs.remove(id);
+            // }
         }
-        cg.delete().map_err(|e| e.to_string())?;
+        // cg.delete().map_err(|e| e.to_string())?;
         Ok::<String, String>("Finished".to_string())
     });
 
