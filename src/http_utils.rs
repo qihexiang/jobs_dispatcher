2aaa3aadb8405b33c2f90603a9a4eec3e4d91a1d
Hexiang Qi <qihexiang@outlook.com>
2023-09-12 08:59:50 +0800

    use into_response

--- src/http_utils.rs
+++ src/http_utils.rs
@@ -4,7 +4,7 @@ use axum::{
     extract::{ConnectInfo, State},
     http::{Request, StatusCode},
     middleware::Next,
-    response::Response,
+    response::{Response, IntoResponse},
 };
 
 pub async fn client_host_check<B>(
@@ -12,17 +12,17 @@ pub async fn client_host_check<B>(
     ConnectInfo(connect): ConnectInfo<SocketAddr>,
     req: Request<B>,
     next: Next<B>,
-) -> Result<Response, StatusCode> {
+) -> Response {
     let ip_addr = connect.ip();
     if allow_list.len() == 0 || allow_list.contains(&ip_addr.to_string()) {
-        Ok(next.run(req).await)
+        next.run(req).await
     } else if let Ok(hostname) = dns_lookup::lookup_addr(&ip_addr) {
         if allow_list.contains(&hostname) {
-            Ok(next.run(req).await)
+            next.run(req).await
         } else {
-            Err(StatusCode::FORBIDDEN)
+            (StatusCode::FORBIDDEN, "hostname not in allow list").into_response()
         }
     } else {
-        Err(StatusCode::FORBIDDEN)
+        (StatusCode::FORBIDDEN, "ip can't resovle and not in allow list").into_response()
     }
 }
