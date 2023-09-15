c39d002c3586a05a35ccc2f953c672eee68ceb56
Hexiang Qi <qihexiang@outlook.com>
2023-09-16 00:23:07 +0800

    now_to_micros

--- src/utils.rs
+++ src/utils.rs
@@ -3,3 +3,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub fn now_to_secs() -> u64 {
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
 }
+
+pub fn now_to_micros() -> u128 {
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros()
+}
